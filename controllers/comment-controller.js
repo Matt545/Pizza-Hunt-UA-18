171733986a69a874b38675208eecaf9382db34aa
Matt545 <84997670+Matt545@users.noreply.github.com>
2022-04-01 17:14:24 -0700

    added reply functionality

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,5 @@
 const { Comment, Pizza } = require('../models');
+const { findOneAndUpdate } = require('../models/Pizza');
 
 const commentController = {
   // add comment to pizza
@@ -20,6 +21,32 @@ const commentController = {
         res.json(dbPizzaData);
       })
       .catch(err => res.json(err));
+  },
+
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      {_id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => res.json(err));
+  },
+
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => res.json(err));
   },
 
   // remove comment
